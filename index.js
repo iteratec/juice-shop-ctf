068b552104a96fe64649d44c2d5bafb8df0c9634
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2017-09-13 15:04:42 +0200

    Ported juice-shop-cli from CTFd to fbctf

--- index.js
+++ index.js
@@ -1,79 +1,60 @@
 'use strict'
 var inquirer = require('inquirer')
 var colors = require('colors') // eslint-disable-line no-unused-vars
+var Promise = require('bluebird')
 var secretKey = require('./lib/secretKey')
 var fetchChallenges = require('./lib/fetchChallenges')
-var generateSql = require('./lib/generateSql')
+var generateFbctfImport = require('./lib/generateFbctfImport')
 var writeToFile = require('./lib/writeToFile')
-var options = require('./lib/options')
+
+var printError = function (err) {
+  console.log(err.red)
+}
 
 var juiceShopCtfCli = function () {
   var questions = [
     {
       type: 'input',
       name: 'juiceShopUrl',
-      message: 'Juice Shop URL to retrieve challenges?',
-      default: 'https://juice-shop.herokuapp.com'
+      message: 'Juice Shop URL to retrieve challenges? \n' +
+       'The Challenges will need to have a country and countryCode attribute. \n' +
+       'Start the iteraCTF branch of the iteratec/juice-shop repository to get a juice-shop version which supports this. \n',
+      default: 'http://localhost:3000'
     },
     {
       type: 'input',
       name: 'ctfKey',
       message: 'Secret key <or> URL to ctf.key file?',
-      default: 'https://raw.githubusercontent.com/bkimminich/juice-shop/master/ctf.key'
-    },
-    {
-      type: 'confirm',
-      name: 'deleteBeforeInsert',
-      message: 'DELETE all CTFd Challenges before INSERT statements?',
-      default: true
-    },
-    {
-      type: 'list',
-      name: 'insertHints',
-      message: 'INSERT a text hint along with each CTFd Challenge?',
-      choices: [options.noTextHints, options.freeTextHints, options.paidTextHints],
-      default: 0
-    },
-    {
-      type: 'list',
-      name: 'insertHintUrls',
-      message: 'INSERT a hint URL along with each CTFd Challenge?',
-      choices: [options.noHintUrls, options.freeHintUrls, options.paidHintUrls],
-      default: 0
-    },
-    {
-      type: 'confirm',
-      name: 'selectAfterInsert',
-      message: 'SELECT all CTFd Challenges after INSERT statements?',
-      default: true
+      default:
+              'https://raw.githubusercontent.com/bkimminich/juice-shop/master/ctf.key'
     }
   ]
-
   console.log()
-  console.log('Generate INSERT statements for ' + 'CTFd'.bold + ' (>=1.0.2) with the ' + 'OWASP Juice Shop'.bold + ' challenges')
-  inquirer.prompt(questions).then(function (answers) {
+  console.log(
+      'Generate a Import File for ' +
+          'fbctf'.bold +
+          ' with the ' +
+          'OWASP Juice Shop'.bold +
+          ' challenges'
+  )
+  inquirer.prompt(questions)
+  .then(function (answers) {
+    return Promise.all([
+      secretKey(answers.ctfKey),
+      fetchChallenges(answers.juiceShopUrl)
+    ])
+  }, printError)
+  .then(function ([hmacKey, challenges]) {
+    return generateFbctfImport(challenges, hmacKey)
+  }, printError)
+  .then(function (exportJson) {
+    return writeToFile(JSON.stringify(exportJson))
+  }, printError)
+  .then(function (file) {
+    console.log('Export File written to ' + file)
     console.log()
-    secretKey(answers.ctfKey).then(function (secretKey) {
-      fetchChallenges(answers.juiceShopUrl).then(function (challenges) {
-        generateSql(challenges, answers.deleteBeforeInsert, answers.insertHints, answers.insertHintUrls, answers.selectAfterInsert, secretKey).then(function (sql) {
-          writeToFile(sql).then(function (file) {
-            console.log('SQL written to ' + file)
-            console.log()
-            console.log('For a step-by-step guide to apply the INSERT statements to ' + 'CTFd'.bold + ', please refer to')
-            console.log('https://github.com/bkimminich/juice-shop-ctf#populating-the-ctfd-database'.bold)
-          }, function (error) {
-            console.log(error.red)
-          })
-        }, function (error) {
-          console.log(error.red)
-        })
-      }, function (error) {
-        console.log(error.red)
-      })
-    }, function (error) {
-      console.log(error.red)
-    })
-  })
+    console.log('Have fun!'.bold + ' 🎉🎉🎉 ')
+  }, printError)
 }
 
 module.exports = juiceShopCtfCli
